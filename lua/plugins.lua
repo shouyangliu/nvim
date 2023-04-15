aa4b4b6eeb62d23f64099a22d356b320cf473368
yangliu <964034804@qq.com>
2023-04-16 01:04:16 +0800

    add treesitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,6 +1,11 @@
 return require('packer').startup(function()
+    -- packer self
 	use 'https://www.gitclone.com/github.com/wbthomason/packer.nvim'
+
+    -- theme
 	use 'https://www.gitclone.com/github.com/junegunn/seoul256.vim' --theme
+
+    --nvim tree
 	use {
 		'https://www.gitclone.com/github.com/kyazdani42/nvim-tree.lua',
 		requires = 'https://www.gitclone.com/github.com/kyazdani42/nvim-web-devicons'}
@@ -9,6 +14,8 @@ return require('packer').startup(function()
     use 'https://www.gitclone.com/github.com/williamboman/mason-lspconfig.nvim'
     use 'https://www.gitclone.com/github.com/p00f/clangd_extensions.nvim'
 	use 'https://www.gitclone.com/github.com/neovim/nvim-lspconfig'
+
+    -- cmp
 	use 'https://www.gitclone.com/github.com/hrsh7th/cmp-nvim-lsp'
 	use 'https://www.gitclone.com/github.com/hrsh7th/cmp-buffer'
 	use 'https://www.gitclone.com/github.com/hrsh7th/cmp-path'
@@ -25,4 +32,6 @@ return require('packer').startup(function()
     use 'https://www.gitclone.com/github.com/airblade/vim-gitgutter'
     -- view git diff
     use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
+    -- 代码高亮插件
+    use { 'https://www.gitclone.com/github.com/nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
     end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -125,6 +125,10 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/yang/.local/share/nvim/site/pack/packer/start/nvim-tree.lua"
   },
+  ["nvim-treesitter"] = {
+    loaded = true,
+    path = "/home/yang/.local/share/nvim/site/pack/packer/start/nvim-treesitter"
+  },
   ["nvim-web-devicons"] = {
     loaded = true,
     path = "/home/yang/.local/share/nvim/site/pack/packer/start/nvim-web-devicons"
